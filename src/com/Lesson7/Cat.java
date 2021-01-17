4f6b6cb58e94c210eac3c83af09db3e56889e385
TroninDmitry <dima-tronin@mail.ru>
2021-01-17 13:05:52 +0300

    HT is ended

--- src/com/Lesson7/Cat.java
+++ src/com/Lesson7/Cat.java
@@ -1,5 +1,33 @@
 package com.Lesson7;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety=false;
+    }
+    public void eat(Plate p) {
+        if (satiety==true) System.out.println("I'm not hungry");
+        else{
+            if (p.getFood()-appetite<0){
+                System.out.println("Cat "+ name+" say: Its too little for me...");
+            } else
+            {
+                System.out.println("Cat "+ name+" say: Omnomnom");
+                p.decreaseFood(appetite);
+                satiety=true;
+            }
+        }
+    }
+    public void getSatiety(){
+        if (satiety==true){
+            System.out.println("Cat "+ name+" say: I'm full");
+        } else if (satiety==false){
+            System.out.println("Cat "+ name+" say: I'm hungry");
+        }
+    }
 
 }
--- src/com/Lesson7/FoodSupplier.java
+++ src/com/Lesson7/FoodSupplier.java
@@ -1,6 +1,30 @@
 package com.Lesson7;
 
 public class FoodSupplier {
+    int valueOfFood;
+    public FoodSupplier(int valueOfFood){
+        this.valueOfFood=valueOfFood;
+    }
+
+    private int isCorrectValueOfFood(Plate plate,int foodValue) {
+        int correctValue=0;
+        if (this.valueOfFood >= foodValue) {
+        if( foodValue+plate.getFood()<=plate.getCAPACITY()){
+            correctValue=foodValue;
+        } else {
+            correctValue= plate.getCAPACITY()-plate.getFood();
+        }
+        }else {
+            correctValue=valueOfFood;
+        }
+        return correctValue;
+    }
+
+    public void putFoodToPlate(Plate plate, int foodValue){
+        int correctValue=isCorrectValueOfFood(plate,foodValue);
+        System.out.println("Put "+ correctValue+" grams of food on a plate");
+        valueOfFood-=correctValue;
+        plate.increaseFood(correctValue);
 
     }
 
